afff9a87e8c1047982a5521be41393bc9847655e
vinimundel <111200648+vinimundel@users.noreply.github.com>
2025-12-14 12:16:03 -0300

    Improve comments in config.py
    Refine comments for clarity in config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Carrega variáveis de ambiente (se existirem no .env)
+# Carrega variáveis de ambiente
 load_dotenv()
 
 # Configurações da API CoinGecko
-# Usaremos o endpoint 'markets' para pegar preço, cap de mercado e volume
+# Endpoint 'markets' para pegar preço, cap de mercado e volume
 BASE_URL = "https://api.coingecko.com/api/v3/coins/markets"
 VS_CURRENCY = "usd"
 COIN_IDS = ["bitcoin", "ethereum"] # IDs das criptomoedas a serem monitoradas
